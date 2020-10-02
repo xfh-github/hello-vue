d5f194054d04a945d68950cc0b806762c1c1ea53
xfh-github <2030280200@qq.com>
2020-10-02 16:50:03 +0800

    Initial commit

---
+++ src/views/user/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>个人信息</h1>
+    {{id}}
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  props:['id'],
+  name: "UserProfile"
+}
+</script>
+
+<style scoped>
+
+</style>
